bf5fcd2fd9e113a7145be0f5bf870adf43e80dd1
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-10-11 10:15:14 -0500

    Run this for KDE5, too.

--- config-kde.sh
+++ config-kde.sh
@@ -2,6 +2,9 @@
 
 # Disable Recent Documents tracking.
 # https://forum.kde.org/viewtopic.php?t=119324
+# https://askubuntu.com/questions/957804/increasing-the-number-of-recently-used-items-in-kde
 # ~/.kde/share/apps/RecentDocuments/
+# ~/.local/share/RecentDocuments
 # ~/.local/share/recently-used.xbel
 kwriteconfig --group RecentDocuments --key UseRecent false
+kwriteconfig5 --group RecentDocuments --key UseRecent false
