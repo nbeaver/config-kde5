7d804ae2d3848c2d2badcef4c25cdb9fb479611a
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-10-10 22:19:24 -0500

    Relevant files.

--- config-kde.sh
+++ config-kde.sh
@@ -2,4 +2,6 @@
 
 # Disable Recent Documents tracking.
 # https://forum.kde.org/viewtopic.php?t=119324
+# ~/.kde/share/apps/RecentDocuments/
+# ~/.local/share/recently-used.xbel
 kwriteconfig --group RecentDocuments --key UseRecent false
