cda10892f184a9c3a5ceead75d3cf50095bbf3da
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-12-11 20:10:40 -0600

    Fix the flickering.

--- config-kde.sh
+++ config-kde.sh
@@ -11,3 +11,6 @@ kwriteconfig5 --group RecentDocuments --key UseRecent false
 
 # Use "Recently used" for window sort order, not "Stacking order".
 kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key SwitchingMode 0
+
+# Use Xrender instead of OpenGL to prevent flickering.
+kwriteconfig5 --file ~/.config/kwinrc --group Compositing --key Backend XRender
