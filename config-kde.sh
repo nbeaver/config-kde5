a6121390ebf7a596269490b2433ed93f14b62db1
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-01-03 13:48:55 -0600

    Use relative paths.

--- config-kde.sh
+++ config-kde.sh
@@ -10,22 +10,22 @@ kwriteconfig --group RecentDocuments --key UseRecent false
 kwriteconfig5 --group RecentDocuments --key UseRecent false
 
 # Use "Recently used" for window sort order, not "Stacking order".
-kwriteconfig5 --file ~/.config/kwinrc --group TabBox --key SwitchingMode 0
+kwriteconfig5 --file kwinrc --group TabBox --key SwitchingMode 0
 
 # Use Xrender instead of OpenGL to prevent flickering.
-kwriteconfig5 --file ~/.config/kwinrc --group Compositing --key Backend XRender
+kwriteconfig5 --file kwinrc --group Compositing --key Backend XRender
 # https://bugs.kde.org/show_bug.cgi?id=384660
 # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=854372
 
 # Set the background color to a nice reddish brown.
-kwriteconfig5 --file ~/.config/plasma-org.kde.plasma.desktop-appletsrc \
+kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc \
     --group Containments --group 1 --group Wallpaper --group org.kde.color \
     --group General --key Color "152,39,29"
 
 # Set Thunderbird as the default email reader.
-kwriteconfig5 --file ~/.config/emaildefaults \
+kwriteconfig5 --file emaildefaults \
     --group "Defaults" --key "Profile" "Default"
-kwriteconfig5 --file ~/.config/emaildefaults \
+kwriteconfig5 --file emaildefaults \
     --group "PROFILE_Default" --key "EmailClient" "/usr/bin/thunderbird"
-kwriteconfig5 --file ~/.config/emaildefaults \
+kwriteconfig5 --file emaildefaults \
     --group "PROFILE_Default" --key "TerminalClient" --type "bool" "false"
