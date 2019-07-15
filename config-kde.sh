4cbd432daf1d78395ef0f4f8e294f88bee9ec6d0
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-07-15 14:02:04 -0500

    Turn off extra runners for Application Launcher (kickoff).

--- config-kde.sh
+++ config-kde.sh
@@ -128,3 +128,17 @@ kwriteconfig5 --file kscreenlockerrc \
 kwriteconfig5 --file ksmserverrc \
     --group 'General' --key 'loginMode' 'default'
 # $ kcmshell5 kcmsmserver
+
+
+# Disable "Expand search to bookmarks, files, and emails" in the kickoff search.
+# ~/.config/plasma-org.kde.plasma.desktop-appletsrc
+if test $(kreadconfig5 --file plasma-org.kde.plasma.desktop-appletsrc \
+    --group Containments --group 2 --group Applets --group 3 \
+    --key 'plugin') = 'org.kde.plasma.kickoff'
+then
+    kwriteconfig5 --file plasma-org.kde.plasma.desktop-appletsrc \
+        --group 'Containments' --group 2 --group Applets --group 3 --group Configuration --group General \
+        --key 'useExtraRunners' 'false'
+else
+    printf "Error: cannot find 'org.kde.plasma.kickoff' in 'plasma-org.kde.plasma.desktop-appletsrc'\n"
+fi
